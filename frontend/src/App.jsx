e84f36483c8745367126bd5ff05a0370f4bd669b
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-07-12 02:43:51 +0530

    Admin Dashboard Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import MyWatchList from "./pages/MyWatchList";
 import Favorite from "./pages/Favorite";
 import {Toaster} from 'react-hot-toast';
 import Footer from "./components/Footer";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddMovies from "./pages/admin/AddMovies";
+import ListMovies from "./pages/admin/ListMovies";
 
 
 function App(){
@@ -24,6 +28,11 @@ function App(){
         <Route path='/movies/:id' element={<MovieDetails/>} />
         <Route path='/my-watchlist' element={<MyWatchList/>} />
         <Route path='/favorite' element={<Favorite/>} />
+        <Route path="/admin/*" element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="add-movies" element={<AddMovies/>}/>
+          <Route path="list-movies" element={<ListMovies/>}/>
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer/>}
     </>
---
+++ frontend/src/pages/admin/ListMovies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ListMovies(){
+    return(
+
+        <div>
+
+        </div>
+    )
+}
+
+export default ListMovies;
