beb7a1c1e4bf4d04d8fb1c806a782aec36f5b750
Lakshya Varshney <lakshyavarshney316@gmail.com>
2025-07-13 23:11:10 +0530

    Backend added with Inngest

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { functions, inngest } from './inngest/index.js';
+const app = express();
+const PORT = 3000;
+
+await connectDB();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+//API Routes
+app.get('/', (req, res)=> res.send('Server is Live'));
+app.use('/api/inngest', serve({client: inngest, functions}))
+
+app.listen(PORT, ()=> console.log(`Server started at http://localhost:${PORT}`));
