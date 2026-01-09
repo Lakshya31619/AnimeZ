414e8be145ee67eb9bbc651bfb0d8753116da6c0
Lakshya Varshney <lakshyavarshney316@gmail.com>
2026-01-09 19:26:07 +0530

    [Lakshya]Add.Added showController.js

--- backend/controllers/showController.js
+++ backend/controllers/showController.js
@@ -1,6 +1,12 @@
-import data from '../data.json' assert { type: 'json' };
 import Movie from '../models/Movie.js';
-import Show from '../models/Show.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataPath = path.join(__dirname, '../data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
 export const getNowPlayingMovies = (req, res) => {
     try {
